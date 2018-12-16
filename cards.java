1c9bf220ff7ce0bdb40aceb0cd5ef86fb373cea8
Bogdan Sorbun <31483568+BogdanSorbun@users.noreply.github.com>
2018-12-16 15:33:32 -0600

    Create cards.java

---
+++ cards.java
@@ -0,0 +1,13 @@
+package card_counter;
+
+public class cards {
+	public static void main(String[] args){
+		System.out.println("Hello");
+	}
+	public static String cards(){
+		String[] suits;
+		String[] ranks;
+		suits = new String[]{"Club", "Heart", "Diamond", "Spade"};
+		ranks = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+	}
+}
