ed55222ab2c5f26d66954644421046047c47089e
Bogdan Sorbun <31483568+BogdanSorbun@users.noreply.github.com>
2018-12-16 18:10:31 -0600

    Add files via upload

--- cards.java
+++ cards.java
@@ -1,13 +1,23 @@
 package card_counter;
+import java.util.Arrays;
+import java.util.ArrayList; 
 
 public class cards {
 	public static void main(String[] args){
-		System.out.println("Hello");
+		String[] ranks;
+		ranks = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+		System.out.println(cards());
 	}
 	public static String cards(){
 		String[] suits;
 		String[] ranks;
+
 		suits = new String[]{"Club", "Heart", "Diamond", "Spade"};
 		ranks = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+		ArrayList<String> card = new ArrayList<String>();
+		for (int i=0; i<=ranks.length - 1; i++){
+			card.add(ranks[i]);
+		}
+		return card.get(0);
 	}
 }
