326452a735801cb0abbeb339963a307fdad320ad
Bogdan Sorbun <31483568+BogdanSorbun@users.noreply.github.com>
2018-12-16 22:05:29 -0600

    Add files via upload

--- cards.java
+++ cards.java
@@ -2,16 +2,19 @@ package card_counter;
 import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.ArrayList; 
+import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class cards {
 	public static void main(String[] args){
-		System.out.println(cards());
+		cards();
+		System.out.println(deckShuffle(card));
 	}
-	public static String cards(){
+	public static ArrayList<String> cards(){
 		String[] suits;
 		String[] ranks;
-		int random;
 
 		suits = new String[]{"Club", "Heart", "Diamond", "Spade"};
 		ranks = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
@@ -28,7 +31,13 @@ public class cards {
 		for (int a=0; a<=ranks.length - 1; a++){
 			card.add(MessageFormat.format("{0} of {1}", ranks[a], suits[3]));
 		}
-		random = (int )(Math.random() * 52);
-		return card.get(random);
+		return card;
+	}
+	public void deckShuffle(){
+		this.cards();
+		Collections.shuffle(card);
+        for (int i=0; i<3; i++) {
+            System.out.println(card.get(i));
+        }
 	}
 }
