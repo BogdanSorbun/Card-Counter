1fd1af738692c561da594360b65b17ddfba3c048
Bogdan Sorbun <31483568+BogdanSorbun@users.noreply.github.com>
2018-12-22 15:52:13 -0600

    working deal.java
    Fixed how the cards are dealt.  Still need to work on the part where you can keep asking for more cards.

--- deal.java
+++ deal.java
@@ -1,86 +1,109 @@
 package cards;
 
 import java.util.*;
-/*
- * This deals the cards out.  
- * 
- * NOTE: if you want to keep going with this deck after it runs out.
- * write a while loop where as long as you want to keep the cards coming, they will come.
- * 
- * Example:
- * while (userinput = "y")
- * 	"Keep printing the cards"
- * 
- */
+import java.util.Scanner; 
+
 public class deal {
 	public static void main(String[] args) {
 		deal(1);
 	}
 	public static void deal(int number){
-		int CardsRemaining;
 		int CardValue = 0;
-		int TotalValue = 0;
 		int HighAce = 11;
 		int LowAce = 1;
-		String[] Hand = new String[21];
+		String[] Hand = new String[52 * number];
 		
 		Stack StackedDeck = new Stack();
 		String[] Deck = shuffler.shuffledDeck(number);
-		CardsRemaining = StackedDeck.size();
 		
 		
 		for(String str : Deck){
 			StackedDeck.push(str);
 		}
 		
-		String PoppedCard = StackedDeck.pop().toString();
-		int UsedCard = cards.cardValue(PoppedCard);
 //		gets user input whether they want to keep going or not
-		Scanner reader = new Scanner(System.in);
-		String read = reader.next();
-		while (read == "Y") {
+		Scanner inp = new Scanner(System.in);
+		System.out.println("Y or N: ");
+		int n = inp.nextInt();
+		while (n == 1) {
 			while (!StackedDeck.isEmpty()){
-				for (int Total=0; Total <= 21; Total = Total + CardValue) {
-	//					find the card value of the card
-					CardValue = UsedCard;
-	//					print the card used
-					System.out.println(PoppedCard);
-	//					append to a list 
-					Hand[0] = PoppedCard;
-	//					find the card value
-					CardValue = UsedCard;
-	//					print the card used
-					System.out.println(PoppedCard);
-	//					append to list
-					Hand[1] = PoppedCard;
-	//					This following below shows if A in the list and automatically assign it the high value.
-					if (Arrays.stream(Hand).anyMatch("A"::equals)) {
+				int Total = 0;
+				while (Total <= 21) {
+
+					Hand[0] = StackedDeck.pop().toString();
+					CardValue = cards.cardValue(Hand[0]);
+					Total = Total + CardValue;
+
+					Hand[1] = StackedDeck.pop().toString();
+					CardValue = cards.cardValue(Hand[1]);
+					Total = Total + CardValue;
+					
+				if (Hand[0].contains("A") && !Hand[1].contains("A")) {
 						if (Total <= 10) {
 							Total = Total + HighAce;
-						}
-	//						this should go through the rest of the cards until we hit 21
-					for(Total = Total; Total <= 21; Total = Total + CardValue) {
-	//						append to the list starting with the second poition of the list since we got the cards from the first part
-						for (int i = 2; i <= 21; i++) {
-							CardValue = UsedCard;
-							System.out.println(PoppedCard);
-							Hand[i] = PoppedCard;
-	//							I'm skeptical on the if statement below but we will see
-							if (Hand[i].contains("A")) {
-								Total = Total + HighAce;
-								if (Total > 21) {
-									Total = Total - HighAce;
-									Total = Total + LowAce;
-									if (Total > 21) {
-	//										then its a bust
-									}
-								}
-							}
+							System.out.println(Hand[1] + " and " + Hand[0] + "   Total:" + Total);
+							System.out.println("Blackjack!");
+							break;
 						}
 					}
+					if (Hand[1].contains("A") && !Hand[0].contains("A")) {
+						if (Total <= 10) {
+							Total = Total + HighAce;
+							System.out.println(Hand[1] + " and " + Hand[0] + "   Total:" + Total);
+							System.out.println("Blackjack!");
+							break;
+						}
+					}
+					if (Hand[0].contains("A") && Hand[1].contains("A")) {
+						Total = HighAce + LowAce;
+					}
+					System.out.println(Hand[1] + " and " + Hand[0] + "   Total:" + Total);
+					Scanner hit = new Scanner(System.in);
+					System.out.println("Hit you?");
+					int hits = hit.nextInt();
+
+					while (hits == 1) {
+						
+						Hand[0] = StackedDeck.pop().toString();
+						CardValue = cards.cardValue(Hand[0]);
+						
+						if (Hand[0].contains("A")) {
+							Total = Total + HighAce;
+							if (Total > 21) {
+								Total = Total - HighAce;
+								Total = Total - LowAce;
+								if (Total > 21) {
+									System.out.println("Total:" + Total + "   Card: " + Hand[0]);
+									System.out.println("Bust");
+									Total = 0;
+									break;
+								}
+								if (Total == 21) {
+									System.out.println("BlackJack!");
+									Total = 0;
+									break;
+								}
+							} else {
+								Total = Total;
+							}
+						}
+						Total = Total + CardValue;
+						System.out.println("line 86   Cardvalue:" + CardValue + "   " + "Total: " + Total  + "    Card:" + Hand[0]);
+						if (Total == 21) {
+							System.out.println("BlackJack!");
+							Total = 0;
+							break;
+						}
+						if (Total > 21) {
+							System.out.println("Total:" + Total + "    Card: " + Hand[0]);
+							System.out.println("Bust");
+							Total = 0;
+							break;
+						}
+					}
+				break;
 				}
 			}
 		}
 	}
-}
 }
